a1103e9af8ed42dd2c25f861e3377246d1c003ff
Annabelle Perbet <annabelleperbet@yahoo.fr>
2026-04-20 18:58:49 +0200

    Mise à jour du fichier

--- script.js
+++ script.js
@@ -1,18 +1,12 @@
 // Récupération des différents éléments
 const passwordInput = document.querySelector("#password");
-const passwordLengthValue = document.querySelector("#password-length__value");
-const passwordLengthInput = document.querySelector("#password-length__input");
+const passwordLengthInput = document.querySelector("#length");
 const uppercaseCheckbox = document.querySelector("#uppercase");
 const lowercaseCheckbox = document.querySelector("#lowercase");
 const numbersCheckbox = document.querySelector("#numbers");
 const specialCheckbox = document.querySelector("#special");
 const generatePasswordBtn = document.querySelector("#generate-password__btn");
 const copyPasswordBtn = document.querySelector("#copy-password__btn");
-
-// Ecoute de l'événement "input" sur l'input de type range
-passwordLengthInput.addEventListener("input", (e) => {
-  passwordLengthValue.textContent = e.target.value;
-});
 
 // Ecoute de l'événement "click" sur le bouton
 generatePasswordBtn.addEventListener("click", () => {
@@ -22,35 +16,60 @@ generatePasswordBtn.addEventListener("click", () => {
   const numbers = numbersCheckbox.checked;
   const special = specialCheckbox.checked;
 
+  // Condition if permettant de vérifier que la longueur du mot de passe soit bien comprise entre 8 et 20 caractères
+  if (length < 8 || length > 20) {
+    alert(
+      "La longeur du mot de passe doit être comprise entre 8 et 20 caractères"
+    );
+    return;
+  }
+
   // Si aucun critère n'est choisi
   if (!uppercase && !lowercase && !numbers && !special) {
     alert("Veuillez selectionner au moins un critère !");
     return;
   }
 
-  // Création des variables passsword et specialCharArray
-  let password = "";
-  let specialCharArray = ["_", "@", "%", "$", "&", "%", "@", "_"];
+  // Création de la variables generatedPasssword et de la constante specialCharArray
+  let generatedPassword = "";
+  const specialCharArray = [
+    "_",
+    "@",
+    "%",
+    "$",
+    "&",
+    "%",
+    "!",
+    ",",
+    ".",
+    "?",
+    ";",
+  ];
 
   // Boucle for qui va parcourir la longueur du mot de passe choisie par l'utilisateur
   for (let i = 0; i < length; i++) {
     let r = Math.random();
     if (r > 0.55 && numbers) {
       // La fonction Math.trunc() retourne la troncature entière d'un nombre en retirant sa partie décimale.
-      password += Math.trunc(Math.random() * 9);
+      generatedPassword += Math.trunc(Math.random() * 9);
     } else if (r > 0.4 && uppercase) {
-      password += String.fromCharCode(Math.trunc(Math.random() * 26) + 65);
+      generatedPassword += String.fromCharCode(
+        Math.trunc(Math.random() * 26) + 65
+      );
     } else if (lowercase) {
-      password += String.fromCharCode(Math.trunc(Math.random() * 26) + 97);
+      generatedPassword += String.fromCharCode(
+        Math.trunc(Math.random() * 26) + 97
+      );
     }
     if (r < Math.random() && special) {
-      password +=
+      generatedPassword +=
         specialCharArray[Math.trunc(Math.random() * specialCharArray.length)];
       i++;
     }
   }
 
-  passwordInput.value = password;
+  passwordInput.value = generatedPassword;
+  // console.log(generatedPassword.length);
 });
 
 // Ecoute de l'événement "click" sur le bouton copier
